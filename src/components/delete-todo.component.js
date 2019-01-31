17288bbb84deea400e01ad1517a175865603908a
ayush raj <ayushrajaot@gmail.com>
2019-01-31 23:28:14 +0530

    delete added

--- src/components/delete-todo.component.js
+++ src/components/delete-todo.component.js
@@ -1,5 +1,31 @@
+// import React, { Component } from 'react';
+// export default class DeleteTodo extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <p>Welcome to Delete Todo Component!!</p>
+//             </div>
+//         )
+//     }
+// }
+
+
 import React, { Component } from 'react';
+import axios from 'axios';
+
 export default class DeleteTodo extends Component {
+
+    constructor(props) {
+        super(props);
+        // this.onSubmit = this.onSubmit.bind(this);
+    }
+    componentDidMount() {
+        // e.preventDefault();
+
+        axios.delete('https://metodolist.herokuapp.com/todos/delete/' + this.props.match.params.id)
+            .then(res => console.log(res.data));
+
+    }
     render() {
         return (
             <div>
